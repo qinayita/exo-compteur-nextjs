fffa46b6cc9644c44582995f486caceeb3b79367
qinayita <domisainz94@hotmail.fr>
2023-09-28 15:19:57 +0200

    marche partiellement

--- app/counter.js
+++ app/counter.js
@@ -1,14 +1,26 @@
+"use client";
+
 import React, { useState } from 'react';
 
 function Counter() {
-    const [count, setcount] = useState(0);
+    const [count, setCount] = useState(0);
+
+    const increment = () => {
+        setCount(count + 1);
+    };
+
+    const decrement = () => {
+        if (count > 0) {
+            setCount(count - 1);
+        }
+    };
 
     return (
-        <>
-            <button onClick={() => setcount(count + 1)}>incrémenter</button>
+        <div>
+            <button onClick={increment}>Incrémenter</button>
             <p>{count}</p>
-            <button disabled={count <= 0} onClick={() => setcount(count > 0 ? count - 1 : 0)}>décrementer</button>
-        </>
+            <button onClick={decrement} disabled={count === 0}>Décrémenter</button>
+        </div>
     );
 }
 
